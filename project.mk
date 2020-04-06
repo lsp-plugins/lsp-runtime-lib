78ffa35ad9c010c332abc6e633fabf0fafba6ee4
SadKo <lsp.plugin@gmail.com>
2020-04-06 18:03:42 +0300

    Initial skeleton

---
+++ project.mk
@@ -0,0 +1,13 @@
+# Package version
+ARTIFACT_NAME               = lsp-runtime-lib
+ARTIFACT_VARS               = LSP_RUNTIME_LIB
+ARTIFACT_HEADERS            = lsp-plug.in
+VERSION                     = 0.5.0-devel
+
+# List of dependencies
+TEST_DEPENDENCIES = \
+  LSP_TEST_FW
+
+DEPENDENCIES = \
+  STDLIB \
+  LSP_COMMON_LIB
