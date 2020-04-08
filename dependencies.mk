b341fcc6006e4fd3ffce7f175e77091d469ef516
SadKo <lsp.plugin@gmail.com>
2020-04-08 16:00:13 +0300

    Added lltl-lib to dependencies

--- dependencies.mk
+++ dependencies.mk
@@ -3,6 +3,10 @@ LSP_COMMON_LIB_VERSION     := 1.0.1
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
 
+LSP_LLTL_LIB_VERSION       := 0.5.0
+LSP_LLTL_LIB_NAME          := lsp-lltl-lib
+LSP_LLTL_LIB_URL           := https://github.com/sadko4u/$(LSP_LLTL_LIB_NAME).git
+
 LSP_TEST_FW_VERSION        := 1.0.1
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
