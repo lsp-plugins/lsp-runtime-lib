c30a92ed7e1f37d07e19b09543cfec2a380535a3
SadKo <lsp.plugin@gmail.com>
2020-04-22 20:27:05 +0300

    Implemented PushParser body, need to write test

--- src/main/fmt/obj/PushParser.cpp
+++ src/main/fmt/obj/PushParser.cpp
@@ -113,8 +113,85 @@ namespace lsp
 
         status_t PushParser::parse_document(IObjHandler *handler)
         {
-            // TODO
-            return STATUS_OK;
+            status_t res = STATUS_OK;
+            size_t nobj = 0;
+
+            while ((res = sParser.next()) == STATUS_OK)
+            {
+                // Fetch event
+                const event_t *ev = sParser.current();
+                if (ev == NULL)
+                    return STATUS_BAD_STATE;
+
+                // Analyze event
+                switch (ev->type)
+                {
+                    case EV_OBJECT:
+                        if (nobj++)
+                        {
+                            if ((res = handler->end_object()) != STATUS_OK)
+                                return res;
+                        }
+                        if ((res = handler->begin_object(&ev->name)) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_VERTEX:
+                        if ((res = handler->add_vertex(ev->vertex.x, ev->vertex.y, ev->vertex.z, ev->vertex.w)) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_PVERTEX:
+                        if ((res = handler->add_param_vertex(ev->vertex.x, ev->vertex.y, ev->vertex.z, ev->vertex.w)) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_NORMAL:
+                        if ((res = handler->add_normal(ev->normal.dx, ev->normal.dy, ev->normal.dz, ev->normal.dw)) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_TEXCOORD:
+                        if ((res = handler->add_texture_vertex(ev->texcoord.u, ev->texcoord.v, ev->texcoord.w)) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_FACE:
+                        if ((res = handler->add_face(ev->ivertex.array(), ev->inormal.array(), ev->itexcoord.array(), ev->ivertex.size())) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_LINE:
+                        if ((res = handler->add_line(ev->ivertex.array(), ev->itexcoord.array(), ev->ivertex.size())) != STATUS_OK)
+                            return res;
+                        break;
+
+                    case EV_POINT:
+                        if ((res = handler->add_points(ev->ivertex.array(), ev->ivertex.size())) != STATUS_OK)
+                            return res;
+                        break;
+
+                    default:
+                        return STATUS_UNSUPPORTED_FORMAT;
+                }
+            }
+
+            if (res == STATUS_EOF)
+                res = STATUS_OK;
+
+            // Post-process data
+            if (res == STATUS_OK)
+            {
+                if (nobj)
+                {
+                    if ((res = handler->end_object()) != STATUS_OK)
+                        return res;
+                }
+
+                res = handler->end_of_data();
+            }
+
+            return res;
         }
     } /* namespace obj */
 } /* namespace lsp */
