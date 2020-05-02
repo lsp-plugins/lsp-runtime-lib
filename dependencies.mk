8c0f3bc78b686868906ae364713f2577fa6b5ff7
SadKo <lsp.plugin@gmail.com>
2020-05-02 22:40:19 +0300

    Preparing for release

--- dependencies.mk
+++ dependencies.mk
@@ -1,5 +1,5 @@
 # Variables that describe dependencies
-LSP_COMMON_LIB_VERSION     := 1.0.1
+LSP_COMMON_LIB_VERSION     := 1.0.2
 LSP_COMMON_LIB_NAME        := lsp-common-lib
 LSP_COMMON_LIB_URL         := https://github.com/sadko4u/$(LSP_COMMON_LIB_NAME).git
 
@@ -7,7 +7,7 @@ LSP_LLTL_LIB_VERSION       := 0.5.0
 LSP_LLTL_LIB_NAME          := lsp-lltl-lib
 LSP_LLTL_LIB_URL           := https://github.com/sadko4u/$(LSP_LLTL_LIB_NAME).git
 
-LSP_TEST_FW_VERSION        := 1.0.1
+LSP_TEST_FW_VERSION        := 1.0.2
 LSP_TEST_FW_NAME           := lsp-test-fw
 LSP_TEST_FW_URL            := https://github.com/sadko4u/$(LSP_TEST_FW_NAME).git
 
