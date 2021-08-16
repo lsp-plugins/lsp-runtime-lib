72ac460d1ccabc76430d9cf6651adc586d8b174c
sadko4u <sadko4u@gmail.com>
2021-08-16 23:05:56 +0300

    Version up

--- include/lsp-plug.in/runtime/version.h
+++ include/lsp-plug.in/runtime/version.h
@@ -24,7 +24,7 @@
 
 #define LSP_RUNTIME_LIB_MAJOR       0
 #define LSP_RUNTIME_LIB_MINOR       5
-#define LSP_RUNTIME_LIB_MICRO       10
+#define LSP_RUNTIME_LIB_MICRO       11
 
 #ifdef LSP_RUNTIME_LIB_BUILTIN
     #define LSP_RUNTIME_LIB_EXPORT
