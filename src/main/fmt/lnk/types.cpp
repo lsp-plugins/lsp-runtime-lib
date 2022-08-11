af8fdf8dcee9d23d32842880449780b1ee8d282a
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-08-12 01:11:46 +0300

    Added Shell Link File data structures and test that validates the example file structure

---
+++ src/main/fmt/lnk/types.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-runtime-lib
+ * Created on: 11 авг. 2022 г.
+ *
+ * lsp-runtime-lib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-runtime-lib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-runtime-lib. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <lsp-plug.in/fmt/lnk/types.h>
+
+namespace lsp
+{
+    namespace lnk
+    {
+        LSP_RUNTIME_LIB_PUBLIC
+        const shlink_guid_t SHLINK_FILE_HEADER_GUID =
+        {
+            0x00021401, 0x00000000, 0x000000C0, 0x46000000
+        };
+    } /* namespace lnk */
+} /* namespace lsp */
+
