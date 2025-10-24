c8180205610b135c2d10f4a681bc751f2c4bd1cb
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-10-24 23:19:36 +0300

    Updated module versions in dependencies

--- modules.mk
+++ modules.mk
@@ -25,7 +25,7 @@ LSP_COMMON_LIB_TYPE        := src
 LSP_COMMON_LIB_URL_RO      := https://github.com/lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 LSP_COMMON_LIB_URL_RW      := git@github.com:lsp-plugins/$(LSP_COMMON_LIB_NAME).git
 
-LSP_LLTL_LIB_VERSION       := 1.0.26
+LSP_LLTL_LIB_VERSION       := 1.0.27
 LSP_LLTL_LIB_NAME          := lsp-lltl-lib
 LSP_LLTL_LIB_TYPE          := src
 LSP_LLTL_LIB_URL_RO        := https://github.com/lsp-plugins/$(LSP_LLTL_LIB_NAME).git
